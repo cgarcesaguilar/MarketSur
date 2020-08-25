a67b1db4d70b6b3d923f97017d8e307c67ec7fef
cgarcesaguilar <cgarces.docente@gmail.com>
2020-08-25 16:32:34 -0400

    Mas materiales

---
+++ recursos/script insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO usuarios () VALUES 
+(null, 'Jorge', 'Maldonando', 'https://randomuser.me/api/portraits/men/24.jpg', '+569 78945625', '123456', 'jmaldonado@gmail.com'),
+(null, 'Luis', 'Gutierrez', 'https://randomuser.me/api/portraits/men/34.jpg', '+569 55688625', '654321', 'lgutierrez@gmail.com');
+
+INSERT INTO avisos () VALUES 
+(null, 'Vendo Smartphone MOTO E 4 PLUS', 60000, 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa beatae tempore ducimus ex, adipisci repellat hic mollitia reiciendis rerum.', 'https://imgcdn.larepublica.co/i/768/2019/04/08165145/s101.jpg', '2020:08:01', 2,1),
+(null, 'Vendo Televisor LG 40 pulgadas', 100000, 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa beatae tempore ducimus ex, adipisci repellat hic mollitia reiciendis rerum.', 'https://imgcdn.larepublica.co/i/768/2019/04/08165145/s101.jpg', '2020:08:02', 2,1),
+(null, 'Vendo Hyndai Elantra 2006 1.6', 2500000, 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa beatae tempore ducimus ex, adipisci repellat hic mollitia reiciendis rerum.', 'https://imgcf.ecn.cl/600/7a/7a4c3b19b14cd9c3da1e170c2b7d6edcc906f8f9.bin.jpg', '2020:08:05', 6,2),
+(null, 'Zapatos de hombre talla 40', 35000, 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa beatae tempore ducimus ex, adipisci repellat hic mollitia reiciendis rerum.', 'https://i.pinimg.com/originals/8a/e1/09/8ae1097e4055a4c6287cdd7223724c79.jpg', '2020:08:06', 3,1),
+(null, 'Vendo Toldo de 3x2 mts', 50000, 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa beatae tempore ducimus ex, adipisci repellat hic mollitia reiciendis rerum.', 'https://i.pinimg.com/originals/18/a0/cd/18a0cd5ef8132cff2e197668947f99e5.jpg', '2020:08:08', 1,2)
